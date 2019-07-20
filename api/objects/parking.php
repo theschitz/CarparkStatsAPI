ea7ee8f2931f331642a6c994d1337d1d6011a35c
theschitz <theodore@theschitz.se>
2019-07-20 15:52:39 +0200

    Making params & filters work. Error handling.

--- api/objects/parking.php
+++ api/objects/parking.php
@@ -21,40 +21,74 @@ class Parking {
  
     public function __construct($db){
         $this->conn = $db;
-        $this->fromDT = new DateTime("2000-01-01");
-        $this->toDT = new DateTime("2999-01-01");
+        $this->fromDT = $this->formatDateTime("2000-01-01");
+        $this->toDT = $this->formatDateTime("2999-01-01");
     }
 
-    public function setFilters($filters) {
+    private function formatDateTime(string $dt) {
+        return (new DateTime("{$dt}"))->format("Y-m-d H:i:s");
+    }
+
+    public function setFilters(array $filters) {
         if (array_key_exists("limit", $filters)) {
             if ($filters["limit"] < $this->limit) {
-                $this->limit = $filters["limit"];
+                if ((int)$filters["limit"]) {
+                    $this->limit = (int)$filters["limit"];    
+                } else {
+                    $this->invalidParam("limit", $filters["limit"]);
+                }
             }
         }
         if (array_key_exists("orderby", $filters)) {
-            if (array_key_exists($filters["orderby"], array("name", "occupancy"))) {
+            if (array_key_exists($filters["orderby"], array("name"=>0, "occupancy"=>0))) {
                 $this->orderby = $filters["orderby"];
             }
         }
         if (array_key_exists("fromDatetime", $filters)) {
-            $this->fromDT = $filters["fromDatetime"];
+            try {
+                $this->fromDT = $this->formatDateTime($filters["fromDatetime"]);
+            } catch (Exception $ex) {
+                $this->invalidParam("fromDatetime", $filters["fromDatetime"]);
+            }
+            
+
         }
         if (array_key_exists("toDatetime", $filters)) {
-            $this->toDT = $filters["toDatetime"];
+            try {
+                $this->toDT = $this->formatDateTime($filters["toDatetime"]);
+            } catch (Exception $ex) {
+                $this->invalidParam("toDatetime", $filters["fromDatetime"]);
+            }
         }
         if (array_key_exists("name", $filters)) {
-            $this->parkingName = $filters["name"];
+            #TODO:  Some parking areas has changed their name over the years
+            #       [] Data correction?
+            #       [] Find a good way to store existing names.
+            if (in_array($filters["name"], array("Västra Torget", "Östra Torget", "Järnbäraren", "Atollen", "Biblioteket", "Per Brahe", "Sesam", "Smedjan", "Spira"))) {
+                $this->parkingName = $filters["name"];
+            } else {
+                $this->invalidParam("name", $filters["name"]);
+            }
         }
     }
 
     function read(){
         $query = "SELECT * FROM {$this->table_name}
-                  WHERE (datetime BETWEEN {$this->fromDT} AND {$this->toDT})
-                  AND name LIKE {$this->parkingName}
+                  WHERE (datetime BETWEEN '{$this->fromDT}' AND '{$this->toDT}')
+                  AND name LIKE '{$this->parkingName}'
                   ORDER BY {$this->orderby} LIMIT {$this->limit}";
+        #print_r($query);
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
-    }    
+    }
+    
+    function invalidParam($param, $value) {
+        http_response_code(400);
+        echo json_encode(
+            array("message" => "Parameter {$param} has an invalid value: {$value}.")
+        );
+        die();
+    }
 }
 ?>
