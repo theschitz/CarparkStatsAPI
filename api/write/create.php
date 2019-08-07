7c21ea05846ef21e5541c5ea9429c57f62b71bd9
theschitz <theodore@theschitz.se>
2019-08-07 22:50:33 +0200

    die() if data incomplete. Clean up of old code.

--- api/write/create.php
+++ api/write/create.php
@@ -18,7 +18,8 @@ $parking = new Parking($db);
 $data = json_decode(file_get_contents("php://input"));
 if (data_is_incomplete($data)) {
     http_response_code(400);
-    echo json_encode(array("message" => "Unable to create entry. Data is incomplete."));   
+    echo json_encode(array("message" => "Unable to create entry. Data is incomplete."));
+    die();
 }
 
 foreach ($data as $p) {
@@ -60,9 +61,6 @@ function data_is_incomplete($data) {
 }
 //gammal kod.---->>>>>>>
 
-$datetime = date("Y-m-d H:i:s");
-$date = date("Y-m-d");
-$time = date("H:i:s");
 
 /* Validate Token */
 if(!isset($_GET['token'])) {
@@ -70,40 +68,4 @@ if(!isset($_GET['token'])) {
 	die();
 }
 # TODO: Bring back token?
-/* Validate Token */
-// Ta emot data
-if (!isset($_GET['json'])) {
-        echo('Error: Bad data.');
-        die();
-}
-$in_json = $_GET['json'];
-$in_data = json_decode($in_json, true);
-
-
-// SQL Connect
-$mysqli = new mysqli($host, $username, $password, $dbname);
-if (mysqli_connect_errno()) {
-	printf("Connect failed: %s\n", mysqli_connect_errno());
-	die();
-}
-
-/* change character set to utf8 */
-if (!$mysqli->set_charset("utf8")) {
-    printf("Error loading character set utf8: %s\n", $mysqli->error);
-    die();
-}
-//Loop & Insert Array
-foreach ($in_data as &$parkings) {
-	$query = "INSERT INTO parkering_jkpg (datetime, name, occupancy, maxoccupancy, marginal, hysteres, active) 
-			  VALUES ('{$datetime}','{$parkings['name']}','{$parkings['occupancy']}','{$parkings['maxOccupancy']}','{$parkings['marginal']}','{$parkings['hysteres']}','{$parkings['active']}')";
-	if (!$mysqli->query($query)) {		
-		http_response_code(500);
-		echo json_encode(array("error" => $mysqli->error));
-		die();
-	}
-}
-$mysqli->close();
-http_response_code(200);
-echo 'OK';
-die();
 ?>
